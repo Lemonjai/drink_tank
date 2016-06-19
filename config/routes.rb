e37009a1a7c0ea4171b6af4130e9150f3b05a2a5
chgchg123 <chgchg123@naver.com>
2016-06-18 23:04:49 -0400

    dollar formation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'drinks#index'
+  resources :drinks
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
